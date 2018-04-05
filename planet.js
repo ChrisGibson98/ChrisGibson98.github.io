dd800db85c6b9c4def3aa0adfb7d3d960853c5eb
SergeantKamikaze <christopher.gibson98@gmail.com>
2018-04-05 17:29:37 +1000

    Begining to add zooming. Mouse over to zoom, mouse off to revert.

--- planet.js
+++ planet.js
@@ -1,25 +1,38 @@
 class planet
 {
-  constructor(a, name, x, y, lengthMulti)
+  constructor(a, planetR, name, x, y, lengthMulti, zooms)
+  {
+    this.a = a;
+    this.planetR = planetR;
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.lengthMulti = lengthMulti;
+    this.zoom= zooms;
+  }
+
+  show()
+  {
+    this.x = (w/this.lengthMulti * sin(this.a)) + w/2;
+    this.y = (h/this.lengthMulti * cos(this.a)) + h/2;
+    stroke(255);
+    line(w/2, h/2, this.x, this.y);
+    noStroke();
+    fill(255);
+    ellipse(this.x, this.y, this.planetR);
+  }
+
+  mouseOn(px, py)
+  {
+    let d = dist(px, py, this.x, this.y)
+    if(d < this.planetR/2)
     {
-this.a = a;
-this.name = name;
-this.x = x;
-this.y = y;
-this.lengthMulti = lengthMulti;
-// this.x = (w/this.lengthMulti * sin(this.a)) + w/2;
-// this.y = (this.length * cos(this.a)) + h/2;
-// console.log(this.x + ", " + this.y);
+      this.zooms = 1;
     }
-    show()
-{
-      this.x = (w/this.lengthMulti * sin(this.a)) + w/2;
-      this.y = (h/this.lengthMulti * cos(this.a)) + h/2;
-      stroke(255);
-      line(w/2, h/2, this.x, this.y);
-      noStroke();
-      fill(255);
-      ellipse(this.x, this.y, r/5);
-      console.log(this.a + ", " + dist(w/2, h/2, this.x,this.y));
+    else
+    {
+      this.zooms = 0;
+      // reset();
     }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -3,11 +3,16 @@ var w;
 var h;
 var s1;
 var textConst;
-var projects = ["Langtons Ant", "Salesman", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3", "project 3",];
+var projects = ["Langtons Ant", "Salesman", "Project 3", "Project 3", "Project 3", "Project 3"];
 var planets = [];
 var angle;
+var BGColor;
+var latestZoom;
 function setup()
 {
+	latestZoom = 0;
+	BGColor = 25;
+	isZoom = 0;
 	angleMode(DEGREES);
 	if (windowWidth > windowHeight)
 	{
@@ -23,33 +28,82 @@ function setup()
 	textConst = r/3.6203611;
 	angle = 360/projects.length;
 	createCanvas(windowWidth, windowHeight);
-	background(25);
+	background(BGColor);
+	for(var i = 0; i < projects.length; i++)
+	{
+		planets[i] = new planet(angle*i, r/5, projects[i], 0, 0, random(2.5, 5), 0);
+		planets[i].show();
+	}
 	fill(255);
 	noStroke();
 	ellipse(w/2, h/2, r);
-	fill(255, 0, 0);
-
-	// rect(w/2 - r, h/2, r*2, 1);
-		// rect(w/2, h/2 - r, 1, r*2);
-		// rect(w/2 - r, h/2 - 8, r*2, 1);
-		for(var i = 0; i < projects.length; i++)
-		{
-			planets[i] = new planet(angle*i, i, 0, 0, random(2.5, 5));
-			planets[i].show();
-		}
+	textConst = r/(3.6203611 + .5203611);
+	fill(25);
+	textSize(textConst);
+	text(s1, w/2 - r/2 + ((r - textWidth(s1))/2), h/2 + ((textConst)/3));
 }
 
 function draw()
 {
-	textConst = r/3.6203611;
-fill(50);
-textSize(textConst);
-text(s1, w/2 - r/2, h/2 + (textConst/3));
-// console.log(angle);
+	for(var i = 0; i < planets.length; i++)
+	{
+		planets[i].mouseOn(mouseX, mouseY);
+	}
+	for(var i = 0; i < planets.length; i++)
+	{
+		if(planets[i].zooms == 1)
+		{
+			latestZoom = planets[i];
+			if(latestZoom.zooms == 1)
+			{
+				planets[i].planetR ++;
+				if(planets[i].planetR > r)
+				{
+					planets[i].planetR = r;
+				}
+				// console.log(planets[i].zooms);
+				colorMode(RGB)
+				fill(255);
+				ellipse(planets[i].x,planets[i].y,planets[i].planetR);
+			}
+		}
+	}
+	if (latestZoom.zooms == 0)
+	{
+		reset();
+	}
 }
+
+function keyPressed()
+{
+	reset();
+}
+
+function reset()
+{
+	clear();
+	colorMode(RGB);
+	background(BGColor);
+	for(var i = 0; i < planets.length; i++)
+	{
+		planets[i].planetR = r/5;
+		planets[i].show();
+	}
+	fill(255);
+	noStroke();
+	ellipse(w/2, h/2, r);
+	textConst = r/(3.6203611 + .5203611);
+	fill(25);
+	textSize(textConst);
+	text(s1, w/2 - r/2 + ((r - textWidth(s1))/2), h/2 + ((textConst)/3));
+}
+
+
+
 function windowResized()
 {
-  resizeCanvas(windowWidth, windowHeight);
+	colorMode(RGB);
+	resizeCanvas(windowWidth, windowHeight);
 	clear();
 	w = windowWidth;
 	h = windowHeight;
@@ -61,12 +115,16 @@ function windowResized()
 	{
 		r = windowWidth/4;
 	}
-	background(50, 50, 50);
+	background(BGColor);
 	fill(255);
 	noStroke();
 	ellipse(w/2, h/2, r);
-for(var i = 0; i < planets.length; i++)
-{
-	planets[i].show();
-}
+	for(var i = 0; i < planets.length; i++)
+	{
+		planets[i].show();
+	}
+	textConst = r/(3.6203611 + .5203611);
+	fill(25);
+	textSize(textConst);
+	text(s1, w/2 - r/2 + ((r - textWidth(s1))/2), h/2 + ((textConst)/3));
 }
